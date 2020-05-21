516bd980c763b6810be091e141b151f83a4209e3
mrtsdc <whois.muchlis@gmail.com>
2020-05-21 14:25:59 +0800

    Membuat Login Activity

--- app/src/main/java/com/muchlis/simak/utils/Singleton.kt
+++ app/src/main/java/com/muchlis/simak/utils/Singleton.kt
@@ -2,10 +2,6 @@ package com.muchlis.simak.utils
 
 class Singleton {
     companion object {
-        var isContainerLvlOneNeedUpdate = false
-        var isContainerLvlTwoNeedUpdate = false
-        var isContainerLvlThreeNeedUpdate = false
-        var isContainerDetailNeedUpdate = false
-        var isContainerCheckNeedUpdate = false
+        var isSsimpleExample = false
     }
 }
